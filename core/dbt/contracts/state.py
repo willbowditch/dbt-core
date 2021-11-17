94a021806b30dd5420ac72bcd76c212b4b809f42
Sung Won Chung <sungwonchung3@gmail.com>
2021-11-17 13:54:20 -0600

    add current state

--- core/dbt/contracts/state.py
+++ core/dbt/contracts/state.py
@@ -36,3 +36,22 @@ class PreviousState:
             except IncompatibleSchemaException as exc:
                 exc.add_filename(str(sources_path))
                 raise
+
+# bring in the project class that needs to be instantiated
+# define the target path at this step(how do I consider a different target path? based on dbt_project.yml)
+# the problem I'm facing right now is that the project config is populated AFTER this step
+# the reason this works with previous state is that we manually set the previous state path
+# current state is more difficult because we have to read the project config first before this class is instantiated
+class CurrentState:
+    def __init__(self, path: Path):
+        self.path: Path = path #TODO: fix this by importing target_path later
+        self.sources: Optional[FreshnessExecutionResultArtifact] = None
+
+        sources_path = self.path / 'sources.json'
+        print(sources_path)
+        if sources_path.exists() and sources_path.is_file():
+            try:
+                self.sources = FreshnessExecutionResultArtifact.read(str(sources_path))
+            except IncompatibleSchemaException as exc:
+                exc.add_filename(str(sources_path))
+                raise
