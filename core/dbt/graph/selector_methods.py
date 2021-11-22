08504e4ec218e371a07f41a118ec64e8588c43d8
Sung Won Chung <sungwonchung3@gmail.com>
2021-11-22 09:33:44 -0600

    remove unused import

--- core/dbt/graph/selector_methods.py
+++ core/dbt/graph/selector_methods.py
@@ -26,7 +26,6 @@ from dbt.contracts.state import PreviousState, CurrentState
 from dbt.exceptions import (
     InternalException,
     RuntimeException,
-    warn_or_error,
 )
 from dbt.node_types import NodeType
 
